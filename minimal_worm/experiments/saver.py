4382ed80784c596ba68a25a0562b6d89bbae836e
Lukas <scld@leeds.ac.uk>
2023-06-21 10:32:25 +0100

    Adds T to h5.attrs

--- minimal_worm/experiments/saver.py
+++ minimal_worm/experiments/saver.py
@@ -67,6 +67,8 @@ class Saver(ABC):
         h5 = h5py.File(filepath, 'w')
         h5.attrs['grid_filename'] = PG.filename + '.json'       
         h5.attrs['shape'] = PG.shape        
+        h5.attrs['T'] = PG.base_parameter['T']
+        
         h5.create_dataset('exit_status', shape = len(PG), dtype = float)
         h5.create_dataset('t', data = FS.t)
 
