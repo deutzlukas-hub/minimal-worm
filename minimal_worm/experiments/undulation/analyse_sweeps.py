6c8eb6824259caccf1fed6c6d36c47556f010ca2
Lukas <scld@leeds.ac.uk>
2023-06-21 10:27:35 +0100

    Refactor

--- minimal_worm/experiments/undulation/analyse_sweeps.py
+++ minimal_worm/experiments/undulation/analyse_sweeps.py
@@ -18,29 +18,50 @@ from minimal_worm.experiments import PostProcessor
 from minimal_worm import POWER_KEYS
 from minimal_worm.experiments.undulation import log_dir, sweep_dir
 
-def compute_swimming_speed(h5):
+def compute_average_curvature_norm(h5: h5py, Delta_t: float = 2.0):
+    '''
+    Computes time averaged L2 norm of curvature minus preferred curvature
+    for every simulation in h5
+    '''
+
+    t = h5['t'][:]
+    idx_arr = t >= Delta_t
+        
+    k_norm_arr = h5['FS']['k_norm'][:, idx_arr]
+    k_avg_norm = k_norm_arr.mean(axis = 1)
+    
+    return k_avg_norm.reshape(h5.attrs['shape'])
+
+def compute_swimming_speed(h5: h5py, Delta_t: float):
+    '''
+    Computes swimming speed for every simulation in h5
+    '''
     
     U_arr = np.zeros(h5['FS']['r'].shape[0])
     t = h5['t'][:]
         
     for i, r in enumerate(h5['FS']['r']):
 
-        U_arr[i] = PostProcessor.comp_mean_swimming_speed(r, t, Delta_t = 2)[0]
+        U_arr[i] = PostProcessor.comp_mean_swimming_speed(r, t, Delta_t)[0]
         
     return U_arr.reshape(h5.attrs['shape'])
                 
-def compute_energies(h5):
+def compute_energies(h5: h5py, Delta_t: float = 2.0):
+    '''
+    Computes energy cost  and mechanical work per undulation period 
+    '''
     
-    t = h5['t'][:]
+    t = h5['t'][:]    
+    N_undu = h5.attrs['T'] - Delta_t
     
     E_dict = {}
     
     # Iterate over powers
     for P_key in POWER_KEYS:                        
-        # Energy
         E = np.zeros(h5['FS']['r'].shape[0])        
+        # Iterate over all simulations 
         for i, P in enumerate(h5['FS'][P_key]):            
-            E[i] = PostProcessor.comp_energy_from_power(P, t, 2)
+            E[i] = PostProcessor.comp_energy_from_power(P, t, Delta_t) / N_undu
             
         E_key = PostProcessor.engery_names_from_power[P_key]            
         E_dict[E_key] = E.reshape(h5.attrs['shape'])
@@ -61,10 +82,16 @@ def analyse_a_b(
 
     h5_analysis.attrs.update(h5_raw_data.attrs)
 
-    U = compute_swimming_speed(h5_raw_data)
-    E_dict = compute_energies(h5_raw_data)
+    # Skip first two periods where 
+    # system dynamics are transient
+    Delta_t = 2.0
+            
+    U = compute_swimming_speed(h5_raw_data, Delta_t)
+    E_dict = compute_energies(h5_raw_data, Delta_t)
+    k_norm = compute_average_curvature_norm(h5_raw_data, Delta_t)
     
     h5_analysis.create_dataset('U', data = U)
+    h5_analysis.create_dataset('k_norm', data = k_norm)
    
     grp = h5_analysis.create_group('energies')
     
