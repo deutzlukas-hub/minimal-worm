bdf1b6d971b731d974242f6e6424de368e27fa87
Lukas <scld@leeds.ac.uk>
2023-07-03 12:01:57 +0100

    Adds sweep_A_lam_a_b

--- minimal_worm/experiments/undulation/sweeps.py
+++ minimal_worm/experiments/undulation/sweeps.py
@@ -15,7 +15,7 @@ from parameter_scan import ParameterGrid
 # Local imports
 from minimal_worm.experiments import Sweeper, Saver
 from minimal_worm.experiments.undulation import UndulationExperiment
-from minimal_worm.experiments.undulation import sweep_dir, log_dir, sim_dir, create_storage_dir
+from minimal_worm.experiments.undulation import create_storage_dir
 from minimal_worm.experiments.undulation.analyse_sweeps import analyse_a_b
 
 def default_sweep_parameter():
@@ -100,7 +100,127 @@ def sweep_a_b(argv):
 
     if sweep_param.save_to_storage:
         log_dir, sim_dir, sweep_dir = create_storage_dir()     
+    else:
+        from minimal_worm.experiments.undulation import sweep_dir, log_dir, sim_dir
+           
+    if sweep_param.run:
+        # Run sweep
+        Sweeper.run_sweep(
+            sweep_param.worker, 
+            PG, 
+            UndulationExperiment.stw_control_sequence, 
+            sweep_param.FK,
+            log_dir, 
+            sim_dir, 
+            sweep_param.overwrite, 
+            sweep_param.debug,
+            'UExp')
+
+    PG_filepath = PG.save(log_dir)
+    print(f'Finished sweep! Save ParameterGrid to {PG_filepath}')
+
+    # dt's number of decimal places 
+    # dp = len(str(Decimal(str(model_param.dt))).split('.')[1])  
+        
+    # Run sweep
+    filename = Path(
+        f'raw_data_'
+        f'a_min={a_min}_a_max={a_max}_step_a={a_step}_'
+        f'b_min={b_min}_b_max={b_max}_step_b={b_step}_'
+        f'A={model_param.A}_lam={model_param.lam}_'
+        f'T={model_param.T}_'        
+        f'N={model_param.N}_dt={model_param.dt}.h5')
+    
+    h5_filepath = sweep_dir / filename
+
+    if sweep_param.pool:        
+        Sweeper.save_sweep_to_h5(PG, h5_filepath, sim_dir, sweep_param.FK_pool)
+
+    if sweep_param.analyse:
+        analyse_a_b(h5_filepath)
                 
+    return
+
+
+def sweep_A_lam_a_b(argv):
+    '''
+    Parameter sweep over time scale ratios a and b
+
+    Show that swimming speed and energy are fully captured 
+    by the system input time scale ratios.          
+    '''    
+
+    # parse sweep parameter
+    sweep_parser = default_sweep_parameter()    
+
+    sweep_parser.add_argument('--A', 
+        type=float, nargs=3, default = [2.0, 10.0, 2.0])    
+    sweep_parser.add_argument('--lam', 
+        type=float, nargs=3, default = [0.5, 2.0, 0.5])    
+    sweep_parser.add_argument('--a', 
+        type=float, nargs=3, default = [-2, 3, 1.0])    
+    sweep_parser.add_argument('--b', 
+        type=float, nargs=3, default = [-3, 0, 1.0])    
+    sweep_parser.add_argument('--FK', nargs = '+', 
+        default = [
+            't', 'r', 'theta', 'd1', 'd2', 'd3', 'k', 'sig', 
+            'k_norm', 'sig_norm', 'r_t', 'w', 'k_t', 'sig_t', 
+            'W_dot', 'D_F_dot', 'D_I_dot', 'V_dot']
+        )
+    sweep_parser.add_argument('--FK_pool', nargs = '+', 
+        default = [
+            'r', 'k', 'sig', 'k_norm', 'sig_norm', 'r_t',
+            'W_dot', 'D_F_dot', 'D_I_dot', 'V_dot']
+        )
+
+
+    sweep_param = sweep_parser.parse_known_args(argv)[0]    
+
+    # parse model parameter and convert to dict
+    model_parser = UndulationExperiment.parameter_parser()
+    model_param = model_parser.parse_known_args(argv)[0]
+
+    # print all command-line-arguments assuming that they
+    # are different from the default option 
+    cml_args = {k: v for k, v in vars(model_param).items() 
+        if v != model_parser.get_default(k)}
+    
+    if len(cml_args) != 0: 
+        print(cml_args)
+
+    A_min, A_max = sweep_param.A[0], sweep_param.A[1]
+    A_step = sweep_param.A[2]
+
+    lam_min, lam_max = sweep_param.lam[0], sweep_param.lam[1]
+    lam_step = sweep_param.lam[2]
+
+    a_min, a_max = sweep_param.a[0], sweep_param.a[1]
+    a_step = sweep_param.a[2]
+
+    b_min, b_max = sweep_param.b[0], sweep_param.b[1]
+    b_step = sweep_param.b[2]
+
+    A_param = {'v_min': A_min, 'v_max': A_max + 0.1*a_step, 
+        'N': None, 'step': A_step, 'round': 1}    
+
+    lam_param = {'v_min': lam_min, 'v_max': lam_max + 0.1*lam_step, 
+        'N': None, 'step': lam_step, 'round': 2, 'log': True}    
+
+    a_param = {'v_min': a_min, 'v_max': a_max + 0.1*a_step, 
+        'N': None, 'step': a_step, 'round': 4, 'log': True}    
+
+    b_param = {'v_min': b_min, 'v_max': b_max + 0.1*b_step, 
+        'N': None, 'step': b_step, 'round': 5, 'log': True}
+
+    grid_param = {'A': A_param, 'lam': lam_param, 'a': a_param, 'b': b_param}
+    
+    PG = ParameterGrid(vars(model_param), grid_param)
+
+    if sweep_param.save_to_storage:
+        log_dir, sim_dir, sweep_dir = create_storage_dir()     
+    else:
+        from minimal_worm.experiments.undulation import sweep_dir, log_dir, sim_dir
+           
     if sweep_param.run:
         # Run sweep
         Sweeper.run_sweep(
@@ -143,7 +263,7 @@ if __name__ == '__main__':
         
     parser = ArgumentParser()
     parser.add_argument('-sweep',  
-        choices = ['a_b'], help='Sweep to run')
+        choices = ['a_b', 'A_lam_a_b'], help='Sweep to run')
         
     # Run function passed via command line
     args = parser.parse_known_args(argv)[0]    
