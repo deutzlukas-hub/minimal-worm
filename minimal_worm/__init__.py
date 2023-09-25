58f959c47a958a6482119c6e8be8fb36570f450c
Lukas <scld@leeds.ac.uk>
2023-09-25 11:00:38 +0100

    Adds convenient import

--- minimal_worm/__init__.py
+++ minimal_worm/__init__.py
@@ -1,4 +1,4 @@
 from .worm import Worm
-from .model_parameters import ModelParameter, parameter_parser, physical_to_dimless_parameters
+from .model_parameters import ModelParameter, parameter_parser, physical_to_dimless_parameters, pic_param
 from .frame import Frame, FrameSequence, FRAME_KEYS, POWER_KEYS
 
