3528245a088439b77e529c3d5f32a4cb5effc509
Lukas <scld@leeds.ac.uk>
2023-10-09 14:33:44 +0100

    debugs

--- minimal_worm/experiments/undulation/thesis.py
+++ minimal_worm/experiments/undulation/thesis.py
@@ -151,7 +151,6 @@ def sweep_N_dt_k(argv):
     model_param = model_parser.parse_known_args(argv)[0]
 
     # Customize parameter
-    model_param.pic_on = True
     model_param.Ds_h = 0.01
     model_param.Ds_t = 0.01
     model_param.s0_h = 0.05
@@ -251,7 +250,6 @@ def sweep_dt_k(argv):
     model_param = model_parser.parse_known_args(argv)[0]
 
     # Customize parameter
-    model_param.pic_on = True
     model_param.Ds_h = 0.01
     model_param.Ds_t = 0.01
     model_param.s0_h = 0.05
@@ -324,6 +322,107 @@ def sweep_dt_k(argv):
     
     pass    
     
+def sweep_c_lam(argv):    
+    '''
+    Sweep over input space
+    '''
+
+    # Parse sweep parameter
+    sweep_parser = default_sweep_parameter()    
+
+    sweep_parser.add_argument('--c', 
+        type=float, nargs=3, default = [0.5, 1.5, 0.1])    
+    sweep_parser.add_argument('--lam', 
+        type=float, nargs=3, default = [0.5, 2.0, 0.1])    
+
+
+    sweep_param = sweep_parser.parse_known_args(argv)[0]    
+    
+    # The argumentparser for the sweep parameter has a boolean argument 
+    # for ever frame key and control key which can be set to true
+    # if it should be saved 
+    FK = [k for k in FRAME_KEYS if getattr(sweep_param, k)]    
+    CK = [k for k in CONTROL_KEYS if getattr(sweep_param, k)]
+
+    # Parse model parameter
+    model_parser = UndulationExperiment.parameter_parser()
+    model_param = model_parser.parse_known_args(argv)[0]
+
+    # Customize parameter
+    model_param.Ds_h = 0.01
+    model_param.Ds_t = 0.01
+    model_param.s0_h = 0.05
+    model_param.s0_t = 0.95
+    model_param.use_c = True
+    model_param.T = 5.0
+    model_param.dt_report = 0.01
+    model_param.N_report = 125
+
+    # Print all model parameter whose value has been
+    # set via the command line
+    cml_args = {k: v for k, v in vars(model_param).items() 
+        if v != model_parser.get_default(k)}
+    
+    if len(cml_args) != 0: 
+        print(cml_args)
+        
+    # Create the ParameterGrid over which we want to run
+    # the undulation experiments
+    c_min, c_max = sweep_param.c[0], sweep_param.c[1]
+    c_step = sweep_param.c[2]
+
+    lam_min, lam_max = sweep_param.lam[0], sweep_param.lam[1]
+    lam_step = sweep_param.lam[2]
+
+    c_param = {'v_min': c_min, 'v_max': c_max + 0.1*c_step, 
+        'N': None, 'step': c_step, 'round': 2}    
+
+    lam_param = {'v_min': lam_min, 'v_max': lam_max + 0.1*lam_step, 
+        'N': None, 'step': lam_step, 'round': 2}
+
+    grid_param = {'c': c_param, 'lam': lam_param}
+    
+    PG = ParameterGrid(vars(model_param), grid_param)
+
+    if sweep_param.save_to_storage:
+        log_dir, sim_dir, sweep_dir = create_storage_dir()     
+    else:
+        from minimal_worm.experiments.undulation import sweep_dir, log_dir, sim_dir
+        
+    # Experiments are run using the Sweeper class for parallelization 
+    if sweep_param.run:
+        Sweeper.run_sweep(
+            sweep_param.worker, 
+            PG, 
+            UndulationExperiment.stw_control_sequence, 
+            FK,
+            log_dir, 
+            sim_dir, 
+            sweep_param.overwrite, 
+            sweep_param.debug,
+            'UExp')
+
+    PG_filepath = PG.save(log_dir)
+    print(f'Finished sweep! Save ParameterGrid to {PG_filepath}')
+        
+    # Pool and save simulation results to hdf5
+    filename = Path(
+        f'raw_data_'
+        f'c_min={c_min}_c_max={c_max}_c_step={c_step}_'
+        f'lam_min={lam_min}_lam_max={lam_max}_lam_step={lam_step}_'                
+        f'N={model_param.N}_dt={model_param.dt}_'        
+        f'T={model_param.T}_pic_on={model_param.pic_on}.h5')    
+    
+    h5_filepath = sweep_dir / filename
+
+    if sweep_param.pool:        
+        Sweeper.save_sweep_to_h5(PG, h5_filepath, sim_dir, FK, CK)
+
+    if sweep_param.analyse:
+        sweep_param.R = True
+        analyse(h5_filepath, what_to_calculate=sweep_param)    
+    return
+    
 def sweep_a_b(argv):
     '''
     Parameter sweep over time scale ratios a and b
@@ -353,18 +452,13 @@ def sweep_a_b(argv):
     model_param = model_parser.parse_known_args(argv)[0]
 
     # Customize parameter
-    model_param.pic_on = True
     model_param.Ds_h = 0.01
     model_param.Ds_t = 0.01
     model_param.s0_h = 0.05
     model_param.s0_t = 0.95
     model_param.use_c = True
     model_param.c = 1.0 
-    model_param.dt = 0.01
-    model_param.N = 250
     model_param.T = 5.0
-    model_param.dt_report = 0.01
-    model_param.N_report = 125
     
     # Print all model parameter whose value has been
     # set via the command line
@@ -387,12 +481,8 @@ def sweep_a_b(argv):
 
     b_param = {'v_min': b_min, 'v_max': b_max + 0.1*b_step, 
         'N': None, 'step': b_step, 'round': 5, 'log': True}
-
-    grid_param = {'a': a_param, 'b': b_param}
     
-    PG = ParameterGrid(vars(model_param), grid_param)
-
-    grid_param = {'a': a_param, 'b': b_param} #'fdo': k_param}
+    grid_param = {'a': a_param, 'b': b_param}
     
     sweep_parser = default_sweep_parameter()    
         
@@ -422,7 +512,10 @@ def sweep_a_b(argv):
     # Pool and save simulation results to hdf5
     filename = Path(
         f'raw_data_'
+        f'a_min={a_min}_a_max={a_max}_a_step={a_step}_'
+        f'b_min={b_min}_b_max={b_max}_b_step={b_step}_'                
         f'c={model_param.c}_lam={model_param.lam}_'
+        f'N={model_param.N}_dt={model_param.dt}_'        
         f'T={model_param.T}_pic_on={model_param.pic_on}.h5')
     
     h5_filepath = sweep_dir / filename
@@ -439,7 +532,7 @@ if __name__ == '__main__':
         
     parser = ArgumentParser()
     parser.add_argument('-sweep',  
-        choices = ['N_dt_k', 'k'], help='Sweep to run')
+        choices = ['N_dt_k', 'c_lam'], help='Sweep to run')
             
     # Run function passed via command line
     args = parser.parse_known_args(argv)[0]    
