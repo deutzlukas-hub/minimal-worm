5d8a1eeef496bdfcf67cd002db2e7f89a399a6b0
LukasDeutz <scld@leeds.ac.uk>
2023-11-29 19:47:42 +0000

    updates

--- minimal_worm/experiments/undulation/plot_modulation.py
+++ minimal_worm/experiments/undulation/plot_modulation.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from parameter_scan import ParameterGrid
 
 # Local import
-from minimal_worm.experiments.undulation.dirs import create_storage_dir
+from minimal_worm.experiments.undulation.dirs import create_storage_dir, fig_dir
 from minimal_worm.experiments import PostProcessor
 
 log_dir, sim_dir, sweep_dir = create_storage_dir()
@@ -26,16 +26,19 @@ def load_data(filename):
     PG_filepath = log_dir / h5.attrs['grid_filename']
     PG = ParameterGrid.init_pg_from_filepath(str(PG_filepath))
 
+
     return h5, PG
 
 def plot_wobbling():
-    
-    h5 = ('raw_data_a_min=1.0_b_min=0.0032_f_range=2.5_'
-        'c_min=0.4_c_max=1.6_c_step=0.1_'
-        'lam_min=0.5_lam_max=2.0_lam_step=0.1_'
-        'N=750_dt=0.01_T=5.0_pic_on=False.h5')
-    
-    h5, PG = load_data(h5)
+
+    h5_fn = ('raw_data_a=1.0_b=0.0032_'
+        'c_min=0.4_c_max=1.6_c_step=0.2_'
+        'lam_min=0.5_lam_max=2.0_lam_step=0.2_'
+        'N=250_dt=0.01_T=5.0_pic_on=False.h5'
+    )
+
+        
+    h5, PG = load_data(h5_fn)
     
     lam0_arr = PG.v_from_key('lam')
     c0_arr = PG.v_from_key('lam')
@@ -43,7 +46,12 @@ def plot_wobbling():
     r = h5['FS']['r']
     t = h5['t'][:]
         
+    plot_dir = fig_dir / 'modulation' / 'wobbling'    
+    plot_dir.mkdir(exist_ok = True, parents = True)       
+    
     for k, r in enumerate(r):
+    
+        print(f'plot={k}')
     
         i, j = np.unravel_index(k, PG.shape)
         
@@ -64,33 +72,133 @@ def plot_wobbling():
         plt.suptitle(f'lam0={lam0_arr[i]}, c0={c0_arr[j]}')              
         plt.plot(r_com[:, 1], r_com[:, 2], '-', c='k')
         plt.plot(r_com_avg[1], r_com_avg[2], 'o', c='r') 
-        plt.plot([r_com_0[1], r_com_1[1]] , [r_com_0[2], r_com_0[1]], '--', c='r') 
-        plt.plot([r_com_2[1], r_com_3[1]] , [r_com_2[2], r_com_3[1]], '--', c='r') 
-
-        plt.show()
-        
-return
+        plt.plot([r_com_0[1], r_com_1[1]] , [r_com_0[2], r_com_1[2]], '--', c='r') 
+        plt.plot([r_com_2[1], r_com_3[1]] , [r_com_2[2], r_com_3[2]], '--', c='r') 
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.axis('equal')
+                
+        plt.savefig(plot_dir / f'{str(k).zfill(3)}.png')                
+        plt.close(plt.gcf())
+            
+    return
 
 def plot_propulsive_force():
+        
+    h5_fn = ('raw_data_a=1.0_b=0.0032_'
+        'c_min=0.4_c_max=1.6_c_step=0.2_'
+        'lam_min=0.5_lam_max=2.0_lam_step=0.2_'
+        'N=250_dt=0.01_T=5.0_pic_on=False.h5'
+    )
     
-    h5 = ('raw_data_a_min=1.0_b_min=0.0032_f_range=2.5_'
-        'c_min=0.4_c_max=1.6_c_step=0.1_'
-        'lam_min=0.5_lam_max=2.0_lam_step=0.1_'
-        'N=750_dt=0.01_T=5.0_pic_on=False.h5')
+    h5, PG = load_data(h5_fn)
+        
+    lam0_arr = PG.v_from_key('lam')
+    c0_arr = PG.v_from_key('c')
     
-    h5, PG = load_data(h5)
+    r_arr = h5['FS']['r'][:]
+    f_F_arr = h5['FS']['f_F'][:]
+    t = h5['t'][:]
+    dt = t[1] - t[0]
+    T = h5.attrs['T']
+                
+    plot_dir = fig_dir / 'modulation' / 'wobbling'    
+    plot_dir.mkdir(exist_ok = True, parents = True)
+               
+    Fp_arr = np.zeros(r_arr.shape[0])
+
+    plot_dir = fig_dir / 'modulation' / 'propulsion_force'    
+    plot_dir.mkdir(exist_ok = True, parents = True)
+               
+    for k, (r, f_F) in enumerate(zip(r_arr, f_F_arr)):
+        
+        print(f'plot={k}')
+        
+        i,j = np.unravel_index(k, PG.shape)
+        lam0 = lam0_arr[j]
+        c0 = c0_arr[i]
+                 
+        Fp, _, t_crop = PostProcessor.comp_propulsive_force(f_F, r, t, Delta_t = T-1)
+        plt.plot(t_crop, Fp)
+        plt.xlabel(r'$t$')
+        plt.ylabel(r'$f_{\mathrm{p}}$')
+        plt.suptitle(fr'$\lambda={lam0}$, $c={c0}$')
+        
+        plt.savefig(plot_dir / f'{str(k).zfill(3)}.png')                
+        plt.close(plt.gcf())
+          
+        Fp_arr[k] = np.trapz(Fp, dx=dt)
+                            
+    Fp_arr = Fp_arr.reshape(PG.shape)
     
-    pass
-    
-    
+    CS = plt.contourf(lam0_arr, c0_arr, Fp_arr, cmap = 'inferno')    
+    plt.contour(lam0_arr, c0_arr, Fp_arr, c='k')
+    plt.colorbar(CS)    
+    plt.show()
+                         
     return
+
+
+def plot_swimming_speed():
+        
+    h5_fn = ('raw_data_a=1.0_b=0.0032_'
+        'c_min=0.4_c_max=1.6_c_step=0.2_'
+        'lam_min=0.5_lam_max=2.0_lam_step=0.2_'
+        'N=250_dt=0.01_T=5.0_pic_on=False.h5'
+    )
+    
+    h5, PG = load_data(h5_fn)
+        
+    lam0_arr = PG.v_from_key('lam')
+    c0_arr = PG.v_from_key('c')
+    
+    r_arr = h5['FS']['r'][:]
+    f_F_arr = h5['FS']['f_F'][:]
+    t = h5['t'][:]
+    T = h5.attrs['T']
+                
+    plot_dir = fig_dir / 'modulation' / 'wobbling'    
+    plot_dir.mkdir(exist_ok = True, parents = True)
+               
+    fp_arr = np.zeros(r_arr.shape[0])
+
+    plot_dir = fig_dir / 'modulation' / 'propulsion_force'    
+    plot_dir.mkdir(exist_ok = True, parents = True)
+               
+    for k, (r, f_F) in enumerate(zip(r_arr, f_F_arr)):
+        
+        print(f'plot={k}')
+        
+        i,j = np.unravel_index(k, PG.shape)
+        lam0 = lam0_arr[j]
+        c0 = c0_arr[i]
+                 
+        avg_fp, _, t_crop = PostProcessor.comp_propulsive_force(f_F, r, t, Delta_t = T-1)
+        # plt.plot(t_crop, avg_fp)
+        # plt.xlabel(r'$t$')
+        # plt.ylabel(r'$f_{\mathrm{p}}$')
+        # plt.suptitle(fr'$\lambda={lam0}$, $c={c0}$')
+        #
+        # plt.savefig(plot_dir / f'{str(k).zfill(3)}.png')                
+        # plt.close(plt.gcf())
+          
+        fp_arr[k] = avg_fp.mean()
+                            
+    fp_arr = fp_arr.reshape(PG.shape)
+    
+    CS = plt.contourf(lam0_arr, c0_arr, fp_arr, cmap = 'inferno')    
+    plt.contour(lam0_arr, c0_arr, fp_arr, c='k')
+    plt.colorbar(CS)    
+    plt.show()
+                         
+    return
+
 
 
 if __name__ == '__main__':
     
-    pass
-    
-    
+    #plot_wobbling() 
+    plot_propulsive_force()    
     
     
 
