b8a247d9868fa0b42e63ed3ac1306753f37c7397
Lukas <scld@leeds.ac.uk>
2024-02-09 11:21:40 +0000

    sweep to fit E

--- minimal_worm/experiments/undulation/application.py
+++ minimal_worm/experiments/undulation/application.py
@@ -412,6 +412,7 @@ def sweep_a_b_water_fang_yen(argv):
     if len(cml_args) != 0: 
         print(cml_args)
     
+    
     # Create the ParameterGrid over which we want to run
     # the undulation experiments
     a_min, a_max = sweep_param.a[0], sweep_param.a[1]
@@ -520,10 +521,12 @@ def sweep_c_lam_water_fang_yen(argv):
     # Init ParameterGrid 
     #===============================================================================
         
-    mu_exp = -3
+    log_mu = -3
     _, f_sig_fit, _ = fang_yen_fit()    
-    T_c = 1.0 / f_sig_fit(mu_exp)
+    T_c = 1.0 / f_sig_fit(log_mu)
     model_param.T_c = T_c * ureg.second
+
+    physical_to_dimless_parameters(model_param)    
 
     # Shape-factor and curvature amplitude     
     c_min, c_max = sweep_param.c0[0], sweep_param.c0[1]
@@ -1198,6 +1201,162 @@ def sweep_mu_lam_c_fang_yen(argv):
         
     return
 
+def sweep_mu_lam_c_fang_yen_fit_E(argv):
+    '''
+    Sweeps over
+    
+    - fluid viscosity mu         
+    
+    Frequency f, lam0 and A0 as a function of mu are determined from fit to Fang Yeng data                       
+    '''
+    
+    # Parse sweep parameter
+    sweep_parser = default_sweep_parameter()    
+
+    sweep_parser.add_argument('--mu', 
+        type=float, nargs=3, default = [-3, 1.2, 0.2])        
+    sweep_parser.add_argument('--lam0', 
+        type=float, nargs=3, default = [0.5, 2.0, 0.1])            
+    sweep_parser.add_argument('--c0', 
+        type=float, nargs=3, default = [0.4, 2.0, 0.1])        
+        
+    sweep_param = sweep_parser.parse_known_args(argv)[0]    
+    
+    # The argumentparser for the sweep parameter has a boolean argument 
+    # for ever frame key and control key which can be set to true
+    # if it should be saved 
+    FK = [k for k in FRAME_KEYS if getattr(sweep_param, k)]    
+    CK = [k for k in CONTROL_KEYS if getattr(sweep_param, k)]
+
+    print(f'FK={FK}')
+
+    # Parse model parameter
+    model_parser = UndulationExperiment.parameter_parser()
+    model_param = model_parser.parse_known_args(argv)[0]
+
+    # Customize parameter
+    model_param.Ds_h = 0.01
+    model_param.Ds_t = 0.01
+    model_param.s0_h = 0.05
+    model_param.s0_t = 0.95
+    model_param.T = 5.0    
+    model_param.use_c = True
+    model_param.a_from_physical = True
+    model_param.b_from_physical = True
+        
+    physical_to_dimless_parameters(model_param)    
+        
+    # Print all model parameter whose value has been
+    # set via the command line
+    cml_args = {k: v for k, v in vars(model_param).items() 
+        if v != model_parser.get_default(k)}
+    
+    if len(cml_args) != 0: 
+        print(cml_args)
+    
+    #===============================================================================
+    # Init ParameterGrid 
+    #===============================================================================
+    
+    mu_exp_min, mu_exp_max = sweep_param.mu[0], sweep_param.mu[1]
+    mu_exp_step = sweep_param.mu[2]
+
+    mu_exp_arr = np.arange(mu_exp_min, mu_exp_max + 0.1 * mu_exp_step, mu_exp_step)        
+    mu_arr = 10**mu_exp_arr                
+        
+    _, f_sig_fit, _ = fang_yen_fit()    
+    T_c_arr = 1.0 / f_sig_fit(mu_exp_arr)
+
+    # Shape-factor and curvature amplitude     
+    c_min, c_max = sweep_param.c0[0], sweep_param.c0[1]
+    c_step = sweep_param.c0[2]
+
+    lam_min, lam_max = sweep_param.lam0[0], sweep_param.lam0[1]
+    lam_step = sweep_param.lam0[2]
+    
+    # Take eta estimate from thesis
+    eta = 10**3.35
+    model_param.eta = eta * model_param.eta.units
+        
+    # Set baseline parameter to lowest viscosity and highest frequency
+    mu0, T0 = mu_arr[0], T_c_arr[0]    
+    model_param.T_c = T0 * ureg.second
+    model_param.mu = mu0 * ureg.pascal * ureg.second
+    
+    c_param = {'v_min': c_min, 'v_max': c_max + 0.1*c_step, 
+        'N': None, 'step': c_step, 'round': 2}    
+
+    lam_param = {'v_min': lam_min, 'v_max': lam_max + 0.1*lam_step, 
+        'N': None, 'step': lam_step, 'round': 2}
+         
+    T_c_param = {'v_arr': T_c_arr.tolist(), 'round': 3, 'quantity': 'second'}    
+    mu_param = {'v_arr': mu_arr.tolist(), 'round': 6, 'quantity': 'pascal*second'}
+    
+    grid_param = {  
+        ('T_c', 'mu'): (T_c_param, mu_param),
+        'c': c_param, 
+        'lam': lam_param
+    }
+                 
+    sweep_parser = default_sweep_parameter()    
+        
+    PG = ParameterGrid(vars(model_param), grid_param)
+
+    if sweep_param.save_to_storage:
+        log_dir, sim_dir, sweep_dir = create_storage_dir()     
+    else:
+        from minimal_worm.experiments.undulation import sweep_dir, log_dir, sim_dir
+
+    #=======================================================================
+    # Run experiments 
+    #=======================================================================
+        
+    # Experiments are run using the Sweeper class for parallelization 
+    if sweep_param.run:
+        Sweeper.run_sweep(
+            sweep_param.worker, 
+            PG, 
+            UndulationExperiment.stw_control_sequence, 
+            FK,
+            log_dir, 
+            sim_dir, 
+            sweep_param.overwrite, 
+            sweep_param.debug,
+            'UExp')
+
+    PG_filepath = PG.save(log_dir)
+    print(f'Finished sweep! Save ParameterGrid to {PG_filepath}')
+        
+    log_E = np.log10(PG.base_parameter['E'].magnitude)
+        
+    # Pool and save simulation results to hdf5
+    filename = Path(
+        f'raw_data_'
+        f'mu_min={mu_exp_min}_mu_max={mu_exp_max}_mu_step={mu_exp_step}_'        
+        f'lam_min={lam_min}_lam_max={lam_max}_lam_step={lam_step}_'        
+        f'c_min={c_min}_c_max={c_max}_c_step={c_step}_'                
+        f'E={np.round(log_E, 2)}_xi={np.round(log_xi, 2)}_'        
+        f'N={model_param.N}_dt={model_param.dt}_'                
+        f'T={model_param.T}.h5')
+    
+    h5_filepath = sweep_dir / filename
+
+    if sweep_param.pool:        
+        Sweeper.save_sweep_to_h5(PG, h5_filepath, sim_dir, FK, CK)
+
+    #===============================================================================
+    # Post analysis 
+    #===============================================================================
+    if sweep_param.analyse:
+        sweep_param.A = True
+        sweep_param.lam = True
+        sweep_param.psi = True        
+        analyse(h5_filepath, what_to_calculate=sweep_param)    
+        
+    return
+
+
+
 def sweep_K_mu_fang_yen(argv):
     '''
     Sweeps over
@@ -1353,7 +1512,7 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-sweep',  
         choices = ['mu_fang_yen', 'mu_fang_yen_test', 'a_b_water_fang_yen', 'c_lam_water_fang_yen',
-        'mu_lam_c_fang_yen', 'E_mu_fang_yen', 'xi_mu_fang_yen',
+        'mu_lam_c_fang_yen', 'mu_lam_c_fang_yen_fit_E', 'E_mu_fang_yen', 'xi_mu_fang_yen',
         'E_xi_mu_fang_yen', 'K_mu_fang_yen'], help='Sweep to run')
                                     
     # Run function passed via command line
--- minimal_worm/experiments/undulation/modulation.py
+++ minimal_worm/experiments/undulation/modulation.py
@@ -656,6 +656,141 @@ def sweep_mu_c_lam(argv):
         
     return
 
+def sweep_E_mu_c_lam(argv):
+    '''
+    Parameter sweep over undulation frequency (a, b), shape-factor c0
+    and undulation wavelength lam0
+    '''    
+    # Parse sweep parameter
+    sweep_parser = default_sweep_parameter()    
+
+    sweep_parser.add_argument('--c', 
+        type=float, nargs=3, default = [0.5, 1.6, 0.1])    
+    sweep_parser.add_argument('--lam', 
+        type=float, nargs=3, default = [0.5, 2.0, 0.1])    
+
+    sweep_param = sweep_parser.parse_known_args(argv)[0]    
+    
+    # The argumentparser for the sweep parameter has a boolean argument 
+    # for ever frame key and control key which can be set to true
+    # if it should be saved 
+    FK = [k for k in FRAME_KEYS if getattr(sweep_param, k)]    
+    CK = [k for k in CONTROL_KEYS if getattr(sweep_param, k)]
+
+    print(f'FK={FK}')
+
+    # Parse model parameter
+    model_parser = UndulationExperiment.parameter_parser()
+    model_param = model_parser.parse_known_args(argv)[0]
+
+    # Customize parameter
+    model_param.Ds_h = 0.01
+    model_param.Ds_t = 0.01
+    model_param.s0_h = 0.05
+    model_param.s0_t = 0.95
+    model_param.T = 5.0
+    model_param.use_c = True
+    
+    # Print all model parameter whose value has been
+    # set via the command line
+    cml_args = {k: v for k, v in vars(model_param).items() 
+        if v != model_parser.get_default(k)}
+    
+    if len(cml_args) != 0: 
+        print(cml_args)
+    
+    #===============================================================================
+    # Init ParameterGrid 
+    #===============================================================================
+    
+    # Undulation frequency     
+    N = int(2e1)
+                        
+    # Choose operating-point for f=f0 and mu=m0
+    mu_range = 4.0                            
+    mu_over_mu0_arr = np.logspace(0.0, mu_range, N)
+                       
+    # Operating point with sigmoidal frequency modulation
+    log_mu_theta = 2.5
+    alpha = 3.0
+    f_min_over_f0 = 0.1
+
+    sigmoid_f_over_f0 = lambda log_mu: f_min_over_f0 + (1 - f_min_over_f0) / (1 + np.exp(alpha*(log_mu - log_mu_theta)))    
+    f_over_f0_arr = sigmoid_f_over_f0(np.log10(mu_over_mu0_arr))
+            
+    a0, b0 = 0.1, 2*10**(-2.0)            
+    f
+    a_mu_arr = a0 * mu_over_mu0_arr * f_over_f0_arr
+    b_mu_arr = b0 * f_over_f0_arr
+    
+    a_param = {'v_arr': a_mu_arr.tolist(), 'round': 3}
+    b_param = {'v_arr': b_mu_arr.tolist(), 'round': 5}
+
+    # Shape-factor and curvature amplitude     
+    c_min, c_max = sweep_param.c[0], sweep_param.c[1]
+    c_step = sweep_param.c[2]
+
+    lam_min, lam_max = sweep_param.lam[0], sweep_param.lam[1]
+    lam_step = sweep_param.lam[2]
+
+    c_param = {'v_min': c_min, 'v_max': c_max + 0.1*c_step, 
+        'N': None, 'step': c_step, 'round': 2}    
+
+    lam_param = {'v_min': lam_min, 'v_max': lam_max + 0.1*lam_step, 
+        'N': None, 'step': lam_step, 'round': 2}
+    
+    grid_param = {
+        ('a', 'b'): (a_param, b_param),
+        'c': c_param, 
+        'lam': lam_param}
+    
+    sweep_parser = default_sweep_parameter()    
+        
+    PG = ParameterGrid(vars(model_param), grid_param)
+
+    if sweep_param.save_to_storage:
+        log_dir, sim_dir, sweep_dir = create_storage_dir()     
+    else:
+        from minimal_worm.experiments.undulation import sweep_dir, log_dir, sim_dir
+        
+    # Experiments are run using the Sweeper class for parallelization 
+    if sweep_param.run:
+        Sweeper.run_sweep(
+            sweep_param.worker, 
+            PG, 
+            UndulationExperiment.stw_control_sequence, 
+            FK,
+            log_dir, 
+            sim_dir, 
+            sweep_param.overwrite, 
+            sweep_param.debug,
+            'UExp')
+
+    PG_filepath = PG.save(log_dir)
+    print(f'Finished sweep! Save ParameterGrid to {PG_filepath}')
+        
+    # Pool and save simulation results to hdf5
+    filename = Path(
+        f'raw_data_'
+        f'a0={a0}_b0={b0}_mu_range={mu_range}_'                
+        f'c_min={c_min}_c_max={c_max}_c_step={c_step}_'
+        f'lam_min={lam_min}_lam_max={lam_max}_lam_step={lam_step}_'
+        f'N={model_param.N}_dt={model_param.dt}_'        
+        f'T={model_param.T}_pic_on={model_param.pic_on}.h5')
+    
+    h5_filepath = sweep_dir / filename
+
+    if sweep_param.pool:        
+        Sweeper.save_sweep_to_h5(PG, h5_filepath, sim_dir, FK, CK)
+
+    if sweep_param.analyse:
+        sweep_param.A = True
+        sweep_param.lam = True
+        sweep_param.psi = True
+        analyse(h5_filepath, what_to_calculate=sweep_param)    
+        
+    return
+
 def sweep_mu_A_lam(argv):
     '''
     Parameter sweep over undulation frequency (a, b), shape-factor c0
