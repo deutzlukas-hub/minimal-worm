0c7612abd42ea6d1040886de49a6b22c4c155332
DeutzLukas <scld@leeds.ac.uk>
2024-07-02 17:43:09 +0100

    added new sweep to test high viscosity

--- minimal_worm/experiments/paper/sweeps.py
+++ minimal_worm/experiments/paper/sweeps.py
@@ -186,6 +186,133 @@ def default_sweep_parameter():
                         dest='fp', help='If true, calculate propulsion force')
     return parser
 
+
+def sweep_lam0_c0(argv):
+    '''
+    Sweeps over
+
+    - fluid viscosity mu
+
+    Frequency f, lam0 and A0 as a function of mu are determined from fit to Fang Yeng data
+    '''
+
+    # Parse sweep parameter
+    sweep_parser = default_sweep_parameter()
+
+    sweep_parser.add_argument('--lam0',
+                              type=float, nargs=3, default=[0.5, 2.0, 0.1])
+    sweep_parser.add_argument('--c0',
+                              type=float, nargs=3, default=[0.4, 2.0, 0.1])
+
+    sweep_param = sweep_parser.parse_known_args(argv)[0]
+
+    #================================================================================================
+    # Customize Model parameter
+    #================================================================================================
+
+    model_parser = UndulationExperiment.parameter_parser()
+    model_param = model_parser.parse_known_args(argv)[0]
+
+    model_param.Ds_h = 0.01
+    model_param.Ds_t = 0.01
+    model_param.s0_h = 0.05
+    model_param.s0_t = 0.95
+    model_param.T = 5.0
+    model_param.use_c = True
+
+    model_param.a_from_physical = True
+    model_param.b_from_physical = True
+
+    # Set xi
+    log_xi = -1.73
+    eta = 10 ** log_xi * model_param.E.magnitude
+    model_param.eta = eta * model_param.eta.units
+
+    # Print all model parameter whose value has been set via the command line
+    cml_args = {k: v for k, v in vars(model_param).items()
+                if v != model_parser.get_default(k)}
+
+    if len(cml_args) != 0:
+        print(cml_args)
+
+    # ===============================================================================
+    # Create Parameter Grid
+    # ===============================================================================
+
+    # Determine undualtion frequency from experimental fit
+    f_sig_fit = fang_yen_fit()[1]
+    T_c = 1.0 / f_sig_fit(np.log10(model_param.mu.magnitude))
+
+    model_param.T_c = T_c * model_param.T_c.units
+
+    # Waveform sweep
+    c_min, c_max, c_step = sweep_param.c0[0], sweep_param.c0[1], sweep_param.c0[2]
+    lam_min, lam_max, lam_step = sweep_param.lam0[0], sweep_param.lam0[1], sweep_param.lam0[2]
+
+    c_param = {'v_min': c_min, 'v_max': c_max + 0.1 * c_step, 'N': None, 'step': c_step, 'round': 2}
+    lam_param = {'v_min': lam_min, 'v_max': lam_max + 0.1 * lam_step, 'N': None, 'step': lam_step, 'round': 2}
+
+    grid_param = {'c': c_param, 'lam': lam_param}
+    PG = ParameterGrid(vars(model_param), grid_param)
+
+    # =======================================================================
+    # Run experiments
+    # =======================================================================
+
+    # Define which output paramter to save
+    FK = [k for k in FRAME_KEYS if getattr(sweep_param, k)]
+    CK = [k for k in CONTROL_KEYS if getattr(sweep_param, k)]
+
+    # Set output path
+    if sweep_param.save_to_storage:
+        log_dir, sim_dir, sweep_dir = create_storage_dir()
+    else:
+        from minimal_worm.experiments.undulation import sweep_dir, log_dir, sim_dir
+
+    # Experiments are run using the Sweeper class for parallelization
+    if sweep_param.run:
+        Sweeper.run_sweep(
+            sweep_param.worker,
+            PG,
+            UndulationExperiment.stw_control_sequence,
+            FK,
+            log_dir,
+            sim_dir,
+            sweep_param.overwrite,
+            sweep_param.debug,
+            'UExp')
+
+    PG_filepath = PG.save(log_dir)
+    print(f'Finished sweep! Save ParameterGrid to {PG_filepath}')
+
+    # Pool and save simulation results to hdf5
+    filename = Path(
+        f'raw_data_'
+        f'mu={model_param.mu.magnitude}_'
+        f'lam_min={lam_min}_lam_max={lam_max}_lam_step={lam_step}_'
+        f'c_min={c_min}_c_max={c_max}_c_step={c_step}_'
+        f'E={np.round(np.log10(model_param.E.magnitude), 2)}_xi={np.round(log_xi, 2)}_'
+        f'N={model_param.N}_dt={model_param.dt}_'
+        f'T={model_param.T}.h5')
+
+    h5_filepath = sweep_dir / filename
+
+    if sweep_param.pool:
+        Sweeper.save_sweep_to_h5(PG, h5_filepath, sim_dir, FK, CK)
+
+    # ===============================================================================
+    # Post analysis
+    # ===============================================================================
+    if sweep_param.analyse:
+        sweep_param.A = True
+        sweep_param.lam = True
+        sweep_param.psi = True
+        analyse(h5_filepath, what_to_calculate=sweep_param)
+
+    return
+
+
+
 def sweep_mu_lam0_c0(argv):
     '''
     Sweeps over
@@ -235,8 +362,6 @@ def sweep_mu_lam0_c0(argv):
 
     if len(cml_args) != 0:
         print(cml_args)
-
-    sweep_parser = default_sweep_parameter()
 
     # ===============================================================================
     # Create Parameter Grid
@@ -446,7 +571,7 @@ if __name__ == '__main__':
 
     parser = ArgumentParser()
     parser.add_argument('--sweep',
-                        choices=['mu_a_b', 'mu_lam0_c0'], help='Sweep to run')
+                        choices=['mu_a_b', 'lam0_c0', 'mu_lam0_c0'], help='Sweep to run')
 
     # Run function passed via command line
     args = parser.parse_known_args(argv)[0]
